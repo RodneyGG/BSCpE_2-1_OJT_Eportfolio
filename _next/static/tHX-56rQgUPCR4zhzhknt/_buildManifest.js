15364b577ba0ee46b5fe940f128097038fec13c6
RodneyGG <RodneyGG@users.noreply.github.com>
2026-07-18 03:37:04 +0000

    deploy: 95f547366cc30ffdaa0e72eeed7f8f6a9b1e909f

---
+++ _next/static/tHX-56rQgUPCR4zhzhknt/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/BSCpE_2-1_OJT_Eportfolio//_next/:path+",
+        "destination": "/BSCpE_2-1_OJT_Eportfolio/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
