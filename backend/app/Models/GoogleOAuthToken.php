ee1ab8bdc5ba6d7e3f166d46aaf9cf5c7ee19ef5
RodneyGG <rodneyarevalo9963@gmail.com>
2026-07-27 13:51:27 +0800

    Fix GoogleOAuthToken table name

--- backend/app/Models/GoogleOAuthToken.php
+++ backend/app/Models/GoogleOAuthToken.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class GoogleOAuthToken extends Model
 {
+    protected $table = 'google_oauth_tokens';
+
     protected $fillable = [
         'access_token',
         'refresh_token',
